d5c48457aaa446bf96c8b85c9c2362993d630710
Ashwini Rathod <rashwini510@gmail.com>
2022-06-23 19:12:49 +0530

    Added: Welcome to hashtable program

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -6,22 +6,7 @@ public class HashTableMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to hashtable program");
-        Hashtable<String, Integer> hashtable = new Hashtable<String, Integer>();
-        String message = "To be or not to be";
 
-        String[] messageArray = message.toLowerCase().split(" ");
-
-        for (String word : messageArray) {
-            Integer value = hashtable.get(word);
-
-            if (value == null)
-                value = 1;
-            else
-                value = value + 1;
-            hashtable.put(word, value);
-
-        }
-        System.out.println(hashtable);
     }
 }
 
