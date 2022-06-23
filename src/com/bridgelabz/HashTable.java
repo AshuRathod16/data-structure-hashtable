332ba9b01495e409d0757b219303d9ee7dd2a368
Ashwini Rathod <rashwini510@gmail.com>
2022-06-23 19:18:49 +0530

    Added: Welcome to hashtable program

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,12 +1,8 @@
 package com.bridgelabz;
 
-import java.util.Hashtable;
-
-public class HashTableMain {
+public class HashTable {
 
     public static void main(String[] args) {
         System.out.println("Welcome to hashtable program");
-
     }
 }
-
